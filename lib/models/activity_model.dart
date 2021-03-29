c5ec1ff92af38e2299365edbd2a09c6473dbcbac
nouraSt <nouramoub93@gmail.com>
2021-03-29 18:48:29 +0100

    hi

---
+++ lib/models/activity_model.dart
@@ -0,0 +1,18 @@
+class Activity{
+  String ImgUrl;
+  String name;
+  String type;
+  List<String> startTimes;
+  int rating;
+  int price;
+
+
+  Activity({
+    this.ImgUrl,
+    this.name,
+    this.type,
+    this.startTimes,
+    this.rating,
+    this.price
+});
+}
